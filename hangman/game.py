89283d532abe41a2f1b3ec800d43ee6ca90c4d5f
Pinchas Lifshitz <pnl8480537@gmail.com>
2025-10-27 11:56:17 +0200

    finsh validate_guess func

--- hangman/game.py
+++ hangman/game.py
@@ -6,3 +6,12 @@ def init_state(secret: str, max_tries: int) -> dict:
         "wrong_guessed":  0,
         "max_tries": max_tries
     }
+
+def validate_guess (ch: str, guessed: set[str]) -> tuple[bool, str]:
+    if len(ch) == 1 and ch.isalpha():
+        if ch not in guessed:
+            return True , " The guess was successful "
+        else:
+            return False , " The guess was unsuccessful "
+    else:
+        return False , " Invalid character entered "
