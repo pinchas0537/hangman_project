02faec8e107a9f53a683b81a29027d6160ebbe22
Pinchas Lifshitz <pnl8480537@gmail.com>
2025-10-27 11:16:44 +0200

    add init_state func

--- hangman/game.py
+++ hangman/game.py
@@ -1,12 +1,8 @@
-guessed = {}
-wrong_guessed = 0
-display = []
-
 def init_state(secret: str, max_tries: int) -> dict:
     return {
         "secret": secret,
-        "display":[' _' * len(secret)],
-        "guessed":guessed,
-        "wrong_guessed":wrong_guessed,
+        "display": [' _' * len(secret)],
+        "guessed": {},
+        "wrong_guessed":  0,
         "max_tries": max_tries
     }
