48b26cf4fe5103ce3ddcb813a604652b97902793
Pinchas Lifshitz <pnl8480537@gmail.com>
2025-10-27 15:10:09 +0200

    finsh print_status func

--- hangman/io.py
+++ hangman/io.py
@@ -1,2 +1,8 @@
 def prompt_guess() -> str:
     return input("Please enter a letter to guess: ")
+
+def print_status(state: dict) -> None:
+    print(f"""The word is: {state["display"]}\n 
+          All the letters guessed are: {state["guessed"]}
+          The remaining guesses are: {state["max_tries"] - {state["wrong_guessed"]}}
+          """)
