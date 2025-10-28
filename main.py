d30167789b268771e411da71ac4409866ab125af
Pinchas Lifshitz <pnl8480537@gmail.com>
2025-10-28 16:04:10 +0200

    finis the project

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from random import choice
 def play (words: list[str], max_tries: int = 10) -> None:
     word = choice(words)
     state = game.init_state(word,max_tries)
-    io.print_status(state)
+    first_print_status = io.print_status(state)
     while state["wrong_guessed"] < max_tries and game.is_won(state) == False:
         input_ch = io.prompt_guess()
         validate_guess = game.validate_guess(input_ch , state["guessed"], state)
@@ -17,9 +17,9 @@ def play (words: list[str], max_tries: int = 10) -> None:
             state["guessed"].add(input_ch)
             print(validate_guess[1])
             swap_display = game.render_display(state,input_ch)
-        io.print_status(state)
+        print_status = io.print_status(state)
         if game.is_won(state) == True or state["wrong_guessed"] == max_tries:
-            io.print_result(state)
+            print_result = io.print_result(state)
             
             
 if __name__ == "__main__":
